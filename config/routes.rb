a0b6849257b7ed0f88ce1fa4997c4ff3446dc61c
Stiffen Alejandro Macias Quintero <stiffen.macias@tangosource.com>
2013-09-06 07:00:20 -0500

    Implemented sign up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Ticketee::Application.routes.draw do
+  get "users/show"
+  get "users/create"
+  get "users/new"
   root :to => "projects#index"
   resources :projects do
     resources :tickets
   end
+  resources :users
 end
