502cc3f652fd8ba9ef5914eaf6b8cf70c1efb04f
Stiffen Alejandro Macias Quintero <stiffen.tangosource@tangosource.com>
2013-09-09 15:06:43 -0500

    Implement profile pages

---
+++ spec/support/factories/user_factory.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :user do
+    name "username"
+    email "sample@example.com"
+    password "hunter2"
+    factory :admin_user do
+      admin true
+    end
+  end
+end
