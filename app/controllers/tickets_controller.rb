ede8891649a30c146d8238e1620b9173f45e3f9b
Stiffen Alejandro Macias Quintero <stiffen.tangosource@tangosource.com>
2013-09-10 06:28:06 -0500

    Add the ability to attach a file to a ticket

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,15 +1,20 @@
 class TicketsController < ApplicationController
 
-  before_action :require_signin!, except: [:show, :index]
+  #before_action :require_signin!, except: [:show, :index]
   before_action :set_project, only: [:show, :edit, :update, :destroy]
   before_action :set_ticket, only: [:show, :edit, :update, :destroy]
 
   def new
+    @project = Project.find(params[:project_id])
     @ticket = @project.tickets.build
+    #@ticket = Ticket.new(:project_id => @project.id)
   end
 
   def create
+    @project = Project.find(params[:project_id])
     @ticket = @project.tickets.build(ticket_params)
+    @ticket.asset = params[:ticket][:asset]
+    @ticket.user = current_user
     if @ticket.save
       flash[:notice] = "Ticket has been created."
       redirect_to [@project, @ticket]
@@ -20,6 +25,8 @@ class TicketsController < ApplicationController
   end
 
   def show
+    @project = Project.find(params[:project_id])
+    @ticket = @project.tickets.build
   end
 
   def edit
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,9 +1,10 @@
 class Ticket < ActiveRecord::Base
 #before_action :require_signin!, except: [:show, :index]
+  mount_uploader :asset, AssetUploader
   belongs_to :project
   belongs_to :user
   validates :title, presence: true
   validates :description, presence: true,
                           length: { minimum: 10 }
-  mount_uploader :asset, AssetUploader
+  
 end
