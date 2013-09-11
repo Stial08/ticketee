d7df4841cb2ea7be826f219bf624c507e72c6e54
Stiffen Alejandro Macias Quintero <stiffen.tangosource@tangosource.com>
2013-09-11 13:03:38 -0500

    Add remove tag functionality

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,8 +2,7 @@ class TagsController < ApplicationController
   def remove
     @ticket = Ticket.find(params[:ticket_id])
     @tag = Tag.find(params[:id])
-    @ticket.tags -= [@tag] <co id="ch11_409_1"/>
+    @ticket.tags -= [@tag] 
     @ticket.save
-    render :nothing => true
   end
 end
