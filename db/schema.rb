362b73b1bf612a2e141ffe7ac97fe8dd97dfa314
Stiffen Alejandro Macias Quintero <stiffen.macias@tangosource.com>
2013-09-13 11:48:29 -0500

    Moved base_controller.rb and projects_cotroller.rb and changed project.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130912232258) do
+ActiveRecord::Schema.define(version: 20130913155404) do
 
   create_table "assets", force: true do |t|
     t.string   "asset"
@@ -35,7 +35,10 @@ ActiveRecord::Schema.define(version: 20130912232258) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "projects", ["user_id"], name: "index_projects_on_user_id"
 
   create_table "states", force: true do |t|
     t.string "name"
