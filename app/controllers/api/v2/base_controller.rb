db095caa6d3a55eebbe54f7f08b98291a25b66e7
Stiffen Alejandro Macias Quintero <stiffen.macias@tangosource.com>
2013-09-19 14:41:48 -0500

    Added controller to version 2 of API, now with paginator

---
+++ app/controllers/api/v2/base_controller.rb
@@ -0,0 +1,21 @@
+class Api::V2::BaseController < ActionController::Base
+  respond_to :json, :xml
+
+  before_action :authenticate_user!
+  before_filter :check_rate_limit
+
+  private
+
+ def check_rate_limit
+   if current_user.request_count > 10
+     error = { :error => "Rate limit exceeded." }
+     respond_with(error, :status => 403)
+     @user = current_user
+     @user.request_count = 0 
+     @user.save
+   else
+     current_user.increment!(:request_count)
+   end
+ end
+
+end
