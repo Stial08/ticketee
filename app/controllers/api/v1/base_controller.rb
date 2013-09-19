fdf20dfa08f57323d2bd55e53f90c13a2692222c
Stiffen Alejandro Macias Quintero <stiffen.macias@tangosource.com>
2013-09-19 14:38:58 -0500

    Edited Rate Limit

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -6,18 +6,6 @@ class Api::V1::BaseController < ActionController::Base
 
   private
 
-  #def authenticate_user
-    #@current_user ||= User.find_by_id!(session[:user_id]) if session[:user_id] != nil
-    #@current_user = User.find_by_authentication_token(params[:token])
-    #binding.pry
-    #unless @current_user
-      #respond_with({:error => "Token is invalid." })
-    #end
-  #end
- #def current_user
-    #@current_user 
-#end
-
  def check_rate_limit
    if current_user.request_count > 10
      error = { :error => "Rate limit exceeded." }
